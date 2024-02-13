7702fb470f9701f0a5957f4ce3738923e7ff8884
Vladimir Pozov <vovasp@gmail.com>
2024-02-13 12:20:18 +0300

    Fix allowed_hosts destination

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -35,12 +35,12 @@ DEBUG = getenv('DJANGO_DEBUG', '0') == 1
 ALLOWED_HOSTS = [
     "127.0.0.1",
     "0.0.0.0",
-]
+] + getenv('DJANGO_ALLOWED_HOSTS', "").split(",")
 
 INTERNAL_IPS = [
     "127.0.0.1",
     "0.0.0.0",
-] + getenv('DJANGO_ALLOWED_HOSTS', "").split(",")
+]
 
 if DEBUG:
     import socket
